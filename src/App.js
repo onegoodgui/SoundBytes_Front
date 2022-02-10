1c55d51adddd847b4a95479ee770edf500212676
Guilherme Dallagnol Vargas <38301783+onegoodgui@users.noreply.github.com>
2022-02-10 03:14:48 -0300

    Revert "Header"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import styled from "styled-components";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import HomePage from "./pages/homepage";
-import Header from "./components/Header";
-
-const AppStyled = styled.div`
-
-`
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <AppStyled>
-      <Header user={"Alan"} shoppingCartSize={"02"} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}> </Route>
-        </Routes>
-      </BrowserRouter>
-    </AppStyled>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./style/index.css"
-import "./style/reset.css"
+import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
