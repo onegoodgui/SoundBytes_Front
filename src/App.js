84f2811e39d1a4d1b6008c9d2a575bf95dfbd19a
Guilherme Dallagnol Vargas <gui_dvargas@hotmail.com>
2022-02-10 12:00:46 -0300

    First version of front-end pages (SignIn and SignUp). Marks the start of Back-end development

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import SignIn from './pages/Sign-In';
+import SignUp from './pages/SignUp';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { SessionDataProvider } from './contexts/SessionDataContext';
@@ -11,6 +12,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path='/sign-in' element={<SignIn/>}></Route>
+            <Route path='/sign-up' element={<SignUp/>}></Route>
           </Routes>
         </BrowserRouter>
       </SessionDataProvider>
